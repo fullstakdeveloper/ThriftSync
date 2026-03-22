9962e333381211f4a25e7d99bb1462cf3817c5fa
fullstakdeveloper <pupru.edu2k7@gmail.com>
2026-03-21 21:44:54 -0400

    gitingore

--- client.cpp
+++ client.cpp
@@ -18,7 +18,7 @@
 //to compile
 //g++ -std=c++17 -o client client.cpp $(pkg-config --cflags --libs libsodium)
 
-//This is the Client File:
+//this is the Client File:
 //1. It takes local data and send it to the server using chunked streaming
 //3. It send data by encrypting the raw bytes first and creates the key
 
